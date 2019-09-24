a0946611064b1dd594531d79ef8acd69b042aab4
aaron <aaronp110@gmail.com>
2019-09-24 18:43:37 -0400

    added filtering S3 keys by multiple strings

--- sample_consts.py
+++ sample_consts.py
@@ -1,6 +1,6 @@
 PATH = 'path to a folder of folders with at least one tif and json file in each of them or' \
        'where you want to store data from S3, ex. C:/Sample Data/'
 BUCKET_NAME = 'name of the s3 bucket to access for tif and json files'
-KEY_FILTER = 'a string you want to filter keys by, can be blank'
+KEY_FILTER = 'a list of strings you want to filter keys by, can be empty'
 TEST_FILE_PATH = 'path to a test tif file, ex. C:/Sample Data/test/test.tif'
 
